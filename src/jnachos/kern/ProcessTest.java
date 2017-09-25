55ea62735ad9c8be92578395b553cf508a984a5d
Sushanthkengunte <sushanthkengunte@gmail.com>
2017-09-25 11:34:01 -0400

    first commit

---
+++ src/jnachos/kern/ProcessTest.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 1992-1993 The Regents of the University of California.
+ * All rights reserved.  See copyright.h for copyright notice and limitation 
+ * of liability and disclaimer of warranty provisions.
+ *  
+ *  Created by Patrick McSweeney on 12/5/08.
+ */
+package jnachos.kern;
+
+import jnachos.machine.Machine;
+
+/**
+ * Set up a ping-pong between two threads, by forking a thread to 'call'.
+ **/
+public class ProcessTest implements VoidFunctionPtr {
+	/**
+	 * The body for processes to run
+	 * 
+	 * @param pArg
+	 *            is an Integer with an id.
+	 */
+	public void call(Object pArg) {
+		// which is the processes local id
+		
+		//System.out.println(JNachos.getCurrentProcess().getProcessID());
+		/*if(pArg == "fork"){
+			NachosProcess tmp = JNachos.getCurrentProcess();
+			new AddrSpace(JNachos.getCurrentProcess().getSpace());
+			JNachos.getCurrentProcess().restoreUserState();
+			//System.out.println("Join process with ID "+Machine.mRegisters[4]);
+			Machine.run();
+		
+			
+		}/*else if(pArg == "parentRun"){
+			new AddrSpace(JNachos.getCurrentProcess().getSpace());
+			JNachos.getCurrentProcess().restoreUserState();
+			int temp = Machine.readRegister(4);
+			Machine.run();
+		}
+		else{*/
+			String fileName = ((String) pArg).toString();
+			JNachos.startProcess(fileName);
+			
+		//}
+		
+			
+		
+	}
+	
+	public ProcessTest(){
+		
+	}
+	/**
+	 * ThreadTest Set up a ping-pong between two threads, by forking a thread to
+	 * call SimpleThread, and then calling SimpleThread ourselves.
+	 **/
+	public ProcessTest(String fileNames) {
+		
+		Debug.print('t', "Entering SimpleTest");
+		String[] twoFilePath = fileNames.split(",");
+		for(int j = 0;j<twoFilePath.length;j++){
+			//System.out.println(twoFilePath[j]+twoFilePath.length);
+			NachosProcess p = new NachosProcess("forked process" + j);
+			
+			p.fork(this, twoFilePath[j]);
+		}
+		
+	}
+	/*public int putInNewThread(){
+		int childProcessID;
+		NachosProcess childProcess = new NachosProcess("Creating child process");
+		childProcessID = childProcess.getProcessID();
+		childProcess.setSpace(JNachos.getCurrentProcess().getSpace());
+		//Machine.mRegisters[Machine.PCReg] =  Machine.mRegisters[Machine.PCReg] + 4;
+		Machine.increaseTheProgramCounter();
+		
+		
+		//Machine.mRegisters[Machine.NextPCReg] =  Machine.mRegisters[Machine.PCReg] + 4;
+		//Machine.mRegisters[2] = 0;
+		childProcess.saveUserState();
+		childProcess.setSpecificRegister(2, 0);
+		
+		//childProcess.se
+		childProcess.fork(this, "fork");
+		return childProcessID;
+	}
+	public void runParent(NachosProcess parent){
+		parent.runTheResumedProcess(this, "parentRun");
+		
+	}*/
+}
---
+++ src/jnachos/kern/SystemCallHandler.java
@@ -0,0 +1,175 @@
+/**
+ * Copyright (c) 1992-1993 The Regents of the University of California.
+ * All rights reserved.  See copyright.h for copyright notice and limitation 
+ * of liability and disclaimer of warranty provisions.
+ *  
+ *  Created by Patrick McSweeney on 12/5/08.
+ */
+package jnachos.kern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.Mac;
+
+import jnachos.filesystem.JavaFileSystem;
+import jnachos.filesystem.OpenFile;
+import jnachos.machine.*;
+
+/** The class handles System calls made from user programs. */
+public class SystemCallHandler {
+	/** The System call index for halting. */
+	public static final int SC_Halt = 0;
+
+	/** The System call index for exiting a program. */
+	public static final int SC_Exit = 1;
+
+	/** The System call index for executing program. */
+	public static final int SC_Exec = 2;
+
+	/** The System call index for joining with a process. */
+	public static final int SC_Join = 3;
+
+	/** The System call index for creating a file. */
+	public static final int SC_Create = 4;
+
+	/** The System call index for opening a file. */
+	public static final int SC_Open = 5;
+
+	/** The System call index for reading a file. */
+	public static final int SC_Read = 6;
+
+	/** The System call index for writting a file. */
+	public static final int SC_Write = 7;
+
+	/** The System call index for closing a file. */
+	public static final int SC_Close = 8;
+
+	/** The System call index for forking a forking a new process. */
+	public static final int SC_Fork = 9;
+
+	/** The System call index for yielding a program. */
+	public static final int SC_Yield = 10;
+
+	/**
+	 * Entry point into the Nachos kernel. Called when a user program is
+	 * executing, and either does a syscall, or generates an addressing or
+	 * arithmetic exception.
+	 * 
+	 * For system calls, the following is the calling convention:
+	 * 
+	 * system call code -- r2 arg1 -- r4 arg2 -- r5 arg3 -- r6 arg4 -- r7
+	 * 
+	 * The result of the system call, if any, must be put back into r2.
+	 * 
+	 * And don't forget to increment the pc before returning. (Or else you'll
+	 * loop making the same system call forever!
+	 * 
+	 * @pWhich is the kind of exception. The list of possible exceptions are in
+	 *         Machine.java
+	 **/
+	public static void handleSystemCall(int pWhichSysCall) {
+
+		Debug.print('a', "!!!!" + Machine.read1 + "," + Machine.read2 + "," + Machine.read4 + "," + Machine.write1 + ","
+				+ Machine.write2 + "," + Machine.write4);
+
+		switch (pWhichSysCall) {
+		// If halt is received shut down
+		case SC_Halt:
+			Debug.print('a', "Shutdown, initiated by user program.");
+			System.out.println("Arguments Passed are "+Machine.mRegisters[2]+ " "+Machine.mRegisters[4]+" "+Machine.mRegisters[5]+" "+Machine.mRegisters[6]+" "+Machine.mRegisters[7] );
+			Interrupt.halt();
+			break;
+
+		case SC_Exit:
+			// Read in any arguments from the 4th register
+			int arg = Machine.readRegister(4);
+
+			System.out
+					.println("Current Process " + JNachos.getCurrentProcess().getName() + " exiting with code " + arg);
+
+			// Finish the invoking process
+		/*	NachosProcess processSleeping = JNachos.getCurrentProcess().getWaitingProcess();
+			if( processSleeping != null){
+				
+				JNachos.getCurrentProcess().setWaitingProcess(null);
+				processSleeping.setSpecificRegister(2, arg);
+				//new AddrSpace(processSleeping.getSpace());
+				//Scheduler.readyToRun(processSleeping);
+				//JNachos.setProcessToBeDestroyed(this);
+				Scheduler.readyToRun(processSleeping);
+				new AddrSpace(processSleeping.getSpace());
+				processSleeping.restoreUserState();
+				//Machine.run();
+				//NachosProcess temp = JNachos.getCurrentProcess();
+				JNachos.getCurrentProcess().finish();
+				
+				//Machine.mRegisters[Machine.PCReg] =  Machine.mRegisters[Machine.PCReg] + 4;
+				//Machine.run();
+				
+				
+			}else{
+				JNachos.getCurrentProcess().finish();
+			}
+			
+			//JNachos.getCurrentProcess().finish();
+			
+			break;
+			
+		case SC_Fork:
+			//System.out.println("Arguments Passed are "+Machine.mRegisters[2]+ " "+Machine.mRegisters[4]+" "+Machine.mRegisters[5]+" "+Machine.mRegisters[6]+" "+Machine.mRegisters[7] );
+			Debug.print('a', "Create new process.");
+			// create a new child
+			int returnValue = JNachos.getCurrentProcess().Fork();
+			Machine.writeRegister(2, returnValue);
+			//Machine.increaseTheProgramCounter();
+			//System.out.println("Join process with ID "+Machine.mRegisters[4]);
+			JNachos.getCurrentProcess().saveUserState();
+			break;
+		case SC_Join:
+			Debug.print('a', "Join");
+			//JNachos.getCurrentProcess().restoreUserState();
+			System.out.println("Join process with ID "+Machine.mRegisters[4]);
+			// Machine.mRegisters[Machine.PCReg] =  Machine.mRegisters[Machine.PCReg] + 4;
+			//System.out.println("Arguments Passed are "+Machine.mRegisters[2]+ " "+Machine.mRegisters[4]+" "+Machine.mRegisters[5]+" "+Machine.mRegisters[6]+" "+Machine.mRegisters[7] );
+			int returns = JNachos.getCurrentProcess().JoinProcess(Machine.readRegister(4));
+			//Machine.mRegisters[Machine.PCReg] =  Machine.mRegisters[Machine.PCReg] + 4;
+			//Machine.mRegisters[2] = returns;
+			
+			if(returns == 0){
+				JNachos.getCurrentProcess().sleep();
+			}			
+			break;
+		
+		case SC_Exec:
+			Debug.print('a', "Execute the program");
+			NachosProcess something = JNachos.getCurrentProcess();
+			//something.restoreUserState();
+			System.out.println("Arguments Passed are "+Machine.mRegisters[2]+ " "+Machine.mRegisters[4]+" "+Machine.mRegisters[5]+" "+Machine.mRegisters[6]+" "+Machine.mRegisters[7] );
+ 			//int temp =  Machine.mRegisters[Machine.PCReg] + 4;
+ 			int offset = Machine.readRegister(4);
+ 			
+ 			ArrayList<Character> sample = new ArrayList<Character>();
+ 			while(Machine.mMainMemory[offset] != '\0'){
+ 				
+ 				sample.add((char)Machine.mMainMemory[offset]);
+ 				
+ 				offset = offset + 1;
+ 			}
+ 			
+ 			String fileName = new String();
+ 			for(char each : sample){
+ 				fileName = fileName+each;
+ 			}
+ 			System.out.println("Executing file : " + fileName);
+ 			JNachos.startExecuting(fileName);
+ 			Machine.increaseTheProgramCounter();
+			System.out.println("Execute ");
+			break;
+*/
+		default:
+			Interrupt.halt();
+			break;
+		}
+	}
+}
